cc85c9498023596a895d897e151f60baa6f32b23
Corbot5000 <corbet@umich.edu>
2019-04-19 15:07:51 -0400

    updating

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,24 @@
+/*Stickybar Fun*/
+// When the user scrolls the page, execute stickyBar 
+window.onscroll = function () {stickyBar()};
+
+// Get the navbar
+var navigation = document.getElementById("stickynav");
+
+// Get the offset position of the navbar
+var sticky = navigation.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyBar() {
+    if (window.pageYOffset >= sticky) {
+        navigation.classList.add("sticky")
+    } else {
+        navigation.classList.remove("sticky");
+    }
+}
+
+/*Accordion Fun */
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -13,86 +34,15 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+/*Javascript for Typerwriter effect */
+var i = 0;
+var txt = 'Hello, my name is Corbet. I study User Experience Research and Design.'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
     }
 }
-
-/*Javascript for Typerwriter effect */
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-    loop: false
-});
-
-typewriter.typeString('Hello, my name is Corbet. I study User Experience Research and Design.')
-    .start();
-
-/*jQuery Form Submission Handling */
-(function()
-{
-    function after_form_submitted(data)
-    {
-        if(data.result == 'success')
-        {
-            $('form#reused_form').hide();
-            $('#success_message').show();
-            $('#error_message').hide();
-        }
-        else
-        {
-            $('#error_message').append('<ul></ul>');
-
-            jQuery.each(data.errors,function(key,val)
-            {
-                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-            });
-            $('#success_message').hide();
-            $('#error_message').show();
-
-            //reverse the response on the button
-            $('button[type="button"]', $form).each(function()
-            {
-                $btn = $(this);
-                label = $btn.prop('orig_label');
-                if(label)
-                {
-                    $btn.prop('type','submit' );
-                    $btn.text(label);
-                    $btn.prop('orig_label','');
-                }
-            });
-
-        }//else
-    }
-
-	$('#reused_form').submit(function(e)
-      {
-        e.preventDefault();
-
-        $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
-            $btn = $(this);
-            $btn.prop('type','button' );
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-
-
-                    $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json'
-            });
-
-      });
-});
